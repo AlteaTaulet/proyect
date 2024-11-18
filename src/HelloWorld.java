0369a722fbe1c65e5b220b7320ecb0574959846b
altea.tauletrius1 <altea.tauletrius1@ciclosformativosceu.es>
2024-11-19 00:48:37 +0100

    3. Metodo de despedida

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        despedida();
     }
+
+    private static void despedida() {
+        System.out.println("Adios!");
+    }
+
+
 }
